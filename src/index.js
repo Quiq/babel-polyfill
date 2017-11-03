4f47d9018c3a4b36c692e2621a406bc8b43a527b
Andrew Jenkins <andrew.jenkins@centricient.com>
2017-11-03 16:28:54 -0600

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+if (global._babelPolyfill) {
+  // throw new Error("only one instance of @babel/polyfill is allowed");
+  return;
+}
+global._babelPolyfill = true;
+
+import "core-js/shim";
+import "regenerator-runtime/runtime";
