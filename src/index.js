cde8b56a8e36283d21bb13986c5e90e8405178db
Fred Vollmer <fredvollmer@Freds-MacBook-Pro.local>
2019-01-22 17:34:47 -0700

    Don't set polyfill loaded global flag

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-if (window.__quiq__disablePolyfill || global._babelPolyfill) {
+if (window.__quiq__disablePolyfill) {
   // throw new Error("only one instance of @babel/polyfill is allowed");
   return;
 }
-global._babelPolyfill = true;
 
 import "core-js/shim";
 import "regenerator-runtime/runtime";
